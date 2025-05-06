9f31cfe5351aad341cab6cf8848c74a025112883
Mario David <mario@die-davids.de>
2025-05-06 16:05:18 +0200

    create initial project structure (#1)
    Co-authored-by: Mario David <mario.david@faktorzehn.de>

---
+++ src/main/java/de/diedavids/mavguard/MavGuardApplication.java
@@ -0,0 +1,13 @@
+package de.diedavids.mavguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavGuardApplication.class, args);
+	}
+
+}
